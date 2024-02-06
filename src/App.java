83c2a8c98b35ca3af2331eac626c2dcef4205c77
LoreM25 <101198371+LoreM25@users.noreply.github.com>
2024-02-06 17:05:56 -0600

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Deck deck = new Deck();
+
+        // Mezclar el deck
+        deck.shuffle();
+        System.out.println(" ");
+        // Primera carta del deck
+        deck.head();
+        System.out.println(" ");
+        // carta al azar
+        deck.pick();
+        System.out.println(" ");
+        // Repartir una mano de cinco cartas
+        deck.hand();
+        System.out.println(" ");
+
     }
 }
